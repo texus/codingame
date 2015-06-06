34577e2ed5d702513d9d05c6ea2f969b1d35a9f1
galtsib <altukhovg@gmail.com>
2015-06-06 16:55:13 +0500

    Update Power of Thor - Code size.cpp

--- SingePlayerPuzzles/Optimization/Power of Thor - Code size.cpp
+++ SingePlayerPuzzles/Optimization/Power of Thor - Code size.cpp
@@ -1,3 +1,3 @@
 #include<iostream>
 #define O(x)std::cout<<(x);
-int main(){int x,y,X,Y,E;std::cin>>x>>y>>X>>Y;while(1){std::cin>>E;O(y>Y?(Y+=1,"S"):y<Y?(Y-=1,"N"):"")O(x>X?(X+=1,"E"):x<X?(X-=1,"W"):"")O("\n")}}
+main(){int x,y,X,Y;std::cin>>x>>y>>X>>Y;while(1){O(y>Y?(Y++,"S"):y<Y?(Y--,"N"):"")O(x>X?(X++,"E"):x<X?(X--,"W"):"")O("\n")}}
