71818552723b72bc16516bec1c32615d4c283b6e
Esteban Mathia <74156507+estebanmathia@users.noreply.github.com>
2023-11-06 14:48:04 +0100

    More compact Power of Thor - Code size.py

--- Optimization/Power of Thor - Code size.py
+++ Optimization/Power of Thor - Code size.py
@@ -1,2 +1,4 @@
-x,y,X,Y=map(int,input().split())
-while 1:Y,v=(((Y,''),(Y-1,'N'))[y<Y],(Y+1,'S'))[y>Y];X,h=(((X,''),(X-1,'W'))[x<X],(X+1,'E'))[x>X];print(v+h)
+a,b,x,y=map(int,input().split())
+while 1:
+ print((""if y==b else"NS"[y<b])+(""if x==a else"EW"[x>a]))
+ y,x=y-(y>b)+(y<b),x-(x>a)+(x<a)
